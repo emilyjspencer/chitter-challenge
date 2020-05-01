fbf670039ea55b5c71ff807c73dda791dc949a51
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-01 05:20:48 +0100

    User can now post messages and see them on the page

--- lib/messages.rb
+++ lib/messages.rb
@@ -2,26 +2,25 @@ require_relative 'database_connection'
 
 class Messages# updates messages - we need to deal with specific instances - i.e. a single message(peep)
 
-  attr_reader :id, :name, :message, :peep_handle, :timestamp # an instance of a message has an id and a message
+  attr_reader :id, :message, :user_id, :timestamp # an instance of a message has an id and a message
 
-  def initialize(id:, name:, message:, peep_handle:, timestamp:) 
+  def initialize(id:, message:, user_id: ,timestamp:) 
     @id = id
-    @name = name
     @message = message
-    @peep_handle = peep_handle
+    @user_id = user_id
     @timestamp = Time.parse(timestamp).strftime("%I:%M %p") 
   end
 
   def self.all 
     result = DatabaseConnection.query("SELECT * FROM messages;") 
     result.map do |message|
-      Messages.new(id: message['id'], name: message['name'], message: message['message'], peep_handle: message['peep_handle'], timestamp: message['posted_at'])
+      Messages.new(id: message['id'], message: message['message'], user_id: message['user_id'], timestamp: message['posted_at'])
     end.reverse
   end 
 
-  def self.create_message(name:, message:, peep_handle:, timestamp:) 
-    result = DatabaseConnection.query("INSERT INTO messages (name, message, peep_handle) VALUES(' #{name}', '#{message}', '#{peep_handle}') RETURNING id, name, message, peep_handle, posted_at;")
-    Messages.new(id: result[0]['id'], name: result[0]['name'], message: result[0]['message'], peep_handle: result[0]['peep_handle'], timestamp: result[0]['posted_at'])
+  def self.create_message(message:, user_id: ) 
+    result = DatabaseConnection.query("INSERT INTO messages (message, user_id) VALUES('#{message}', '#{user_id}') RETURNING id, message, user_id, posted_at;")
+    Messages.new(id: result[0]['id'], message: result[0]['message'], user_id: result[0]['user_id'], timestamp: result[0]['posted_at'])
   end 
 
 end 
