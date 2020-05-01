78fa38b4315ddfa85edd135cba8e81ecb79dcec5
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-01 06:38:25 +0100

    Test passing for see_messages_spec.rb

--- spec/features/see_messages_spec.rb
+++ spec/features/see_messages_spec.rb
@@ -2,13 +2,15 @@
 
  feature 'See messages' do
   scenario 'user can see messages on the page' do
+    visit ('/users/new')
+     fill_in('email', with: 'emily@gmail.com')
+     fill_in('password', with: 'coding')
+     fill_in('peep_handle', with: 'lajoiedevivre89')
+     click_button('Register')
     connection = PG.connect(dbname: 'chitter_test') 
-    Messages.create_message(name: 'Emily', message: 'We are learning how to build a full-stack application', peep_handle: 'lajoiedevivre89', timestamp: '05:30:07')
-    Messages.create_message(name: 'Billy', message: 'From Monday we will no longer be the newest cohort', peep_handle: 'housemusic1', timestamp: '05:30:07')
-    Messages.create_message(name: 'Jonas', message: 'Tweets sound better than peeps', peep_handle: 'popmusicrocks', timestamp: '05:30:07')
-    visit '/index'
+    user = Users.create(email: 'emily@gmail.com', peep_handle: 'lajoiedevivre89', password: '12345')
+    Messages.create_message(message: 'We are learning how to build a full-stack application', user_id: user.id)
+    visit '/messages'
     expect(page).to have_content 'We are learning how to build a full-stack application' 
-    expect(page).to have_content 'From Monday we will no longer be the newest cohort' 
-    expect(page).to have_content 'Tweets sound better than peeps' 
   end
 end
