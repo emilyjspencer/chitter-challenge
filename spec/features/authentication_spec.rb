6673db390f1dca8ebf449d95a93bbfe0196d8ff6
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-01 05:45:01 +0100

    Test passing for successful login

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -1,12 +1,12 @@
 feature 'User authentication' do
-   it 'a user can sign with the correct credentials' do
+   it 'a user can log in with the correct credentials' do
      Users.create(email: 'emily@gmail,com', password: 'coding', peep_handle: 'lajoiedevivre89')
      visit('/sessions/new')
      fill_in('email', with: 'emily@gmail.com')
      fill_in('password', with: 'coding')
      click_button('Sign in')
      visit('/messages')
-     expect(page).to have_content 'Chitter - for all your Chatter'
+     expect(page).to have_content 'Welcome to Chitter lajoiedevivre89'
    end
 
 
