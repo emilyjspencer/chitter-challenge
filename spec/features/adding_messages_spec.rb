53b6b67f50b6ce214499889cf612efab2ebd5bf3
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-01 05:40:08 +0100

    Test passing for the user seeing a submit button to submit their message

--- spec/features/adding_messages_spec.rb
+++ spec/features/adding_messages_spec.rb
@@ -12,5 +12,19 @@ feature 'Adding message' do
     click_button('Submit')
     expect(page).to have_content('My first message')
   end
+
+  scenario 'a user can see a submit button on their messages page' do
+    visit '/'
+    click_button('Click here to sign up for a Chitter account')
+    visit '/users/new'
+    fill_in('email', with: 'emily@gmail.com')
+    fill_in('peep_handle', with: 'emilyjane')
+    fill_in('password', with: '12345')
+    click_button('Register')
+    visit('/messages')
+    expect(page).to have_button 'Submit'
+  end
 end 
+
+
 
