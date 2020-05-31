4b2d85070f6a22fd2efe10673c7cdd004fb74ade
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-31 21:43:48 +0100

    Slightly modifies User model

--- lib/user.rb
+++ lib/user.rb
@@ -1,14 +1,15 @@
 class User
    attr_reader  :id, :peep_handle, :email
 
-   def initialize(id:, peep_handle:, email: ) # id matches with id messages table  1 to many relationship for users  1 user  many messages
+   def initialize(id:, peep_handle:, email: ) 
      @id = id
      @peep_handle = peep_handle
      @email = email
    end
 
    def self.create(email:, password:, peep_handle:)
-     result = DatabaseConnection.query("INSERT INTO users (email, password, peep_handle) VALUES('#{email}', '#{password}', '#{peep_handle}') RETURNING id, email, peep_handle;")
+     encrypted_password = BCrypt::Password.create(password)
+     result = DatabaseConnection.query("INSERT INTO users (email, password, peep_handle) VALUES('#{email}', '#{encrypted_password}', '#{peep_handle}') RETURNING id, email, peep_handle;")
      User.new(id: result[0]['id'], email: result[0]['email'], handle: result[0]['peep_handle'])
    end
  end
