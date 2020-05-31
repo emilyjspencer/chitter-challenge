59c301ca5be23c4a8834ca000cb4f686f8c6346b
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-31 21:34:02 +0100

    Removes dead code

--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -4,12 +4,12 @@ require 'database_helpers'
  describe Users do
 
    describe '.create' do
-     it 'creates a new user' do # user has an id, email, password and handle
+     it 'creates a new user' do 
        user = Users.create(email: 'emily@gmail.com', password: 'coding', peep_handle: 'lajoiedevivre89')
-       persisted_data = persisted_data(table: :users, id: user.id) # data that is saved is
-       expect(user).to be_a Users # expect it to be an instance of the User class
-       expect(user.id).to eq persisted_data.first['id'] # return that particular id
-       expect(user.email).to eq 'emily@gmail.com' # check that attr_reader id is indeed returning the correct email for test purposes 
+       persisted_data = persisted_data(table: :users, id: user.id) 
+       expect(user).to be_a Users 
+       expect(user.id).to eq persisted_data.first['id'] 
+       expect(user.email).to eq 'emily@gmail.com' 
      end
    
       it 'hides password using the BCrypt gem' do
@@ -36,7 +36,7 @@ require 'database_helpers'
     it 'returns a user when given the correct credentials - email and password' do
       user = Users.create(email: 'emily@gmail.com', password: 'coding', peep_handle: 'lajoiedevivre89')
       authenticated = Users.authenticate(email: 'emily@gmail,com', password: 'coding')
-      expect(authenticated.id).to eq user.id # expect the authenticated user's id to match that same user's id -to ensure its the same person 
+      expect(authenticated.id).to eq user.id 
     end
 
     it 'returns nil when wrong email input' do
